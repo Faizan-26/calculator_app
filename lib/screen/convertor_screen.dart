a0b0750d0236d2a26436873d0c0bfafc726a7908
Faizan Tariq <f223858@cfd.nu.edu.pk>
2024-02-09 18:01:03 +0500

    ui tweaks

--- lib/screen/convertor_screen.dart
+++ lib/screen/convertor_screen.dart
@@ -11,6 +11,8 @@ class ConverterScreen extends StatefulWidget {
 }
 
 class _ConverterScreenState extends State<ConverterScreen> {
+  String upperValue = "1";
+  String lowerValue = "0";
   void btnTaped(String s) {
     print(s);
   }
@@ -19,92 +21,155 @@ class _ConverterScreenState extends State<ConverterScreen> {
     print('C');
   }
 
-// !isNumeric()
-//             ? Theme.of(context).colorScheme.inversePrimary
-//             : Theme.of(context).colorScheme.primaryContainer,
-
+  bool toggleSwap = false; // this is used to swap upper and lower section
   @override
   Widget build(BuildContext context) {
-    Widget upperSection = Material(
-      color: Theme.of(context).colorScheme.primaryContainer,
-      child: InkWell(
-        onTap: () {
-          print('Tapped');
-        },
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-          child: Column(children: [
-            Row(
+    Widget upperSection = InkWell(
+      onTap: () {
+        print('Tapped');
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: Column(children: [
+          Row(
+            children: [
+              Text(
+                "Kilometer",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Icon(
+                Icons.arrow_drop_down,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Row(
+              textDirection: TextDirection.rtl,
               children: [
-                Text(
-                  "Kilometer",
-                  style: Theme.of(context).textTheme.titleMedium,
+                Card(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Km",
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
                 ),
-                Icon(
-                  Icons.arrow_drop_down,
-                  color: Theme.of(context).colorScheme.primary,
+                const SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        upperValue,
+                        overflow: TextOverflow.visible,
+                        maxLines: 1,
+                        style: const TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Row(
-                textDirection: TextDirection.rtl,
-                children: [
-                  Card(
-                    color: Theme.of(context).colorScheme.inversePrimary,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        "Km",
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  const FittedBox(
-                    fit: BoxFit.scaleDown,
-                    child: Text(
-                      "000",
-                      overflow: TextOverflow.visible,
-                      maxLines: 1,
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.w100,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ]),
-        ),
+          )
+        ]),
       ),
     );
 
-    Widget lowerSection = Material(
-      child: InkWell(
-        onTap: () {
-          print('Tapped');
-        },
-        child: const Placeholder(
-          fallbackHeight: 100,
-        ),
+    Widget lowerSection = InkWell(
+      onTap: () {
+        print('Lower Section Tapped');
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: Column(children: [
+          Row(
+            children: [
+              Text(
+                "Meters",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Icon(
+                Icons.arrow_drop_down,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Row(
+              textDirection: TextDirection.rtl,
+              children: [
+                Card(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Km",
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        lowerValue,
+                        overflow: TextOverflow.visible,
+                        maxLines: 1,
+                        style: const TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ]),
       ),
     );
     Widget centralButton = Material(
       // this button is use to swap upper section with lower section
-      child: InkWell(
-        onTap: () {
-          print('Tapped');
-        },
-        child: const Placeholder(
-          fallbackHeight: 50,
+      child: IconButton(
+        icon: Icon(
+          Icons.swap_vert,
+          size: 32,
+          color: Theme.of(context).colorScheme.primary,
         ),
+        onPressed: () {
+          setState(() {
+            String temp = upperValue; // restrict values to swap
+            upperValue = lowerValue;
+            lowerValue = temp;
+            toggleSwap = !toggleSwap;
+          });
+        },
       ),
     );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('${widget.appbarName} Converter'),
@@ -116,11 +181,14 @@ class _ConverterScreenState extends State<ConverterScreen> {
               flex: 1,
               child: Column(
                 children: [
-                  upperSection,
-                  const Placeholder(
-                    fallbackHeight: 100,
+                  Material(
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                    child: !toggleSwap ? upperSection : lowerSection,
                   ),
-                  lowerSection
+                  centralButton,
+                  Material(
+                    child: toggleSwap ? upperSection : lowerSection,
+                  ),
                 ],
               )),
           Expanded(
